92ef1fc6476f72effdd26fcdfebffdcb55fbfc45
Daniel Palonek <danekp0510@vp.pl>
2016-08-19 22:46:49 +0200

    Skeleton of services

---
+++ ClickMapActivity-service/src/main/java/com/academy/service/ActivityService.java
@@ -0,0 +1,13 @@
+package com.academy.service;
+
+import com.academy.model.dao.Activity;
+import com.academy.model.dto.ActivityDTO;
+import com.academy.repo.ActivityRepo;
+import com.academy.service.mappers.ActivityMapper;
+
+/**
+ * Created by Daniel Palonek on 2016-08-19.
+ */
+public class ActivityService extends AbstractService<Activity,ActivityDTO,ActivityRepo,ActivityMapper> {
+
+}
---
+++ ClickMapActivity-service/src/main/java/com/academy/service/WebsiteService.java
@@ -0,0 +1,12 @@
+package com.academy.service;
+
+import com.academy.model.dao.Website;
+import com.academy.model.dto.WebsiteDTO;
+import com.academy.repo.WebsiteRepo;
+import com.academy.service.mappers.WebsiteMapper;
+
+/**
+ * Created by Daniel Palonek on 2016-08-19.
+ */
+public class WebsiteService extends AbstractService<Website,WebsiteDTO,WebsiteRepo,WebsiteMapper> {
+}
