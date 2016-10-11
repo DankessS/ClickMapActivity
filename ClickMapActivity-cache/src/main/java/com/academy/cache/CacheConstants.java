61b5a99b3febfedf8856c23c888514a3fd120aae
Daniel Palonek <danekp0510@vp.pl>
2016-10-11 18:27:19 +0200

    Missing files

---
+++ ClickMapActivity-cache/src/main/java/com/academy/cache/CacheConstants.java
@@ -0,0 +1,12 @@
+package com.academy.cache;
+
+/**
+ * Created by Daniel Palonek on 2016-09-05.
+ */
+public class CacheConstants {
+
+    public static final String LOGGED_USERNAME = "loggedUsername";
+    public static final String USER_WEBSITES = "userWebsites";
+    public static final String REQUESTED_WEBSITE = "requestedWebsite";
+
+}
